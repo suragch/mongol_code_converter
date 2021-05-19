b7e2fb63ab43ef135a54c4122152582f5147eb15
suragch <studymongolian@gmail.com>
2021-05-19 18:03:29 +0800

    android update

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Mongol Code',
       theme: ThemeData(
         primarySwatch: Colors.blue,
